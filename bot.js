812c76cfc02335dc3dc30b63badcadb2e97290f6
rrtor <41769454+rrtor@users.noreply.github.com>
2018-09-04 03:16:21 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -135,6 +135,25 @@ client.on("guildMemberAdd", member => {
       });
 
 
+client.on('message', message => {
+    if(message.content === '!'){
+        message.channel.send('**Welcome To S7Q STORE..:champagne_glass:**')
+    }
+});
+
+client.on("message", message => {
+
+            if (message.content.startsWith(prefix + "bc")) {
+                         if (!message.member.hasPermission("ADMINISTRATOR"))  return;
+  let args = message.content.split(" ").slice(1);
+  var argresult = args.join(' '); 
+  message.guild.members.filter(m => m.presence.status !== 'offline').forEach(m => {
+ m.send(`${argresult}\n ${m}`);
+})
+ message.channel.send(`\`${message.guild.members.filter(m => m.presence.status !== 'online').size}\` : عدد الاعضاء المستلمين`); 
+ message.delete(); 
+};     
+});
 
 
 
